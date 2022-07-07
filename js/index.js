3314d4264b1540e3079f1731b8a57808fe090555
MohammedAtallahhh <mohammedatallahh2017@gmail.com>
2022-07-07 12:41:05 +0200

    Added the drag'n drop functionality

--- js/index.js
+++ js/index.js
@@ -4,23 +4,81 @@ const timeline = document.querySelector(".timeline");
 
 const slideshowWrapper = slideshow.querySelector(".swiper-wrapper");
 const timelineItemsElement = timeline.querySelector(".timeline-items");
+const libraryItemsElement = library.querySelector(".library-items");
 const playButton = slideshow.querySelector(".play");
 const stopButton = slideshow.querySelector(".stop");
 const transitionForm = timeline.querySelector(".timeline-transition");
 
 let swiper;
 
+let libraryImages = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+  "15",
+  "16",
+  "17",
+];
 const timelineItems = new Array(10).fill(0);
-timelineItems[2] = "images/1.jpg";
 let timelineTransition = "slide";
 
 //------------------ FUNCTIONS
+const getNameFromSrc = (src) => src.split("/")[1].split(".")[0];
+const render = () => {
+  renderLibrary();
+  renderTimeline();
+  renderSlideShow();
+  makeSlider();
+};
+
 const renderTimeline = () => {
+  timelineItemsElement.innerHTML = "";
   timelineItems.forEach(
     (item, i) =>
-      (timelineItemsElement.innerHTML += `<div class='timeline-item'>
-          ${item === 0 ? i + 1 : `<img src=${item} />`}  
-    </div>`)
+      (timelineItemsElement.innerHTML += `
+      <div class='timeline-item drop-target' data-index='${i}' draggable>
+          ${item === 0 ? i + 1 : `<img src=${item} />`}
+      </div>`)
+  );
+};
+
+const deleteItemFromTimeline = (e) => {
+  e.preventDefault();
+  if (e.target.closest(".timeline-item")) {
+    const item = e.target.closest(".timeline-item");
+    const itemIndex = item.dataset.index;
+
+    // Making sure that we are not deleting an empty cell
+    if (timelineItems[itemIndex] !== 0) {
+      const src = item.firstElementChild.attributes.src.value;
+      const imgName = getNameFromSrc(src);
+      libraryImages.unshift(imgName);
+      timelineItems[itemIndex] = 0;
+      render();
+    }
+  }
+};
+
+const renderLibrary = () => {
+  libraryItemsElement.innerHTML = "";
+  libraryImages.forEach(
+    (item) =>
+      (libraryItemsElement.innerHTML += `
+    <div class="library-item" draggable>
+      <img src='images/${item}.jpg' />
+    </div>
+  `)
   );
 };
 
@@ -35,7 +93,6 @@ const makeSlider = () => {
 
   swiper = new Swiper(".swiper", {
     effect: timelineTransition,
-    spaceBetween: 50,
     speed: 500,
     autoplay: {
       delay: 1000,
@@ -75,6 +132,7 @@ const makeSlider = () => {
 };
 
 const renderSlideShow = () => {
+  slideshowWrapper.innerHTML = "";
   timelineItems.forEach(
     (slide, i) =>
       (slideshowWrapper.innerHTML += `
@@ -101,9 +159,59 @@ const handleTransitionChange = (e) => {
   }
 };
 
-renderTimeline();
-renderSlideShow();
-makeSlider();
+const handleDragStart = (e) => {
+  swiper.slideTo(0);
+  swiper.autoplay.stop();
+
+  if (!e.target.closest("[draggable]")) return;
+
+  timeline.classList.add("active-dragging");
+  library.classList.add("active-dragging");
+
+  console.dir(e.target);
+  e.dataTransfer.setData(
+    "text/plain",
+    JSON.stringify({
+      src: e.target.attributes.src.value,
+      target: e.target.parentElement.dataset.index,
+    })
+  );
+};
+
+const handleDragEnd = (e) => {
+  timeline.classList.remove("active-dragging");
+};
+
+const handleDrop = (e) => {
+  // If it's not droppable do nothing
+  if (!e.target.closest(".drop-target")) return;
+  e.preventDefault();
+
+  // getting data from the dragged content
+  const data = JSON.parse(e.dataTransfer.getData("text/plain"));
+  const imgSrc = data.src;
+  const imgName = getNameFromSrc(imgSrc);
+  const oldImgIndex = data.target && +data.target;
+
+  const currentImgIndex = +e.target.closest(".drop-target").dataset.index;
+
+  // If we are dragging from the timeline we need to delete the old cell
+  if (oldImgIndex !== undefined) {
+    timelineItems[oldImgIndex] = timelineItems[currentImgIndex];
+  } else libraryImages = libraryImages.filter((img) => img !== imgName);
+
+  timelineItems[currentImgIndex] = imgSrc;
+  render();
+  timeline.classList.remove("active-dragging");
+};
+// render for the first time
+render();
 
 //------------------------- EVENTS
+timeline.addEventListener("contextmenu", deleteItemFromTimeline);
 transitionForm.addEventListener("click", handleTransitionChange);
+document.body.addEventListener("dragstart", handleDragStart);
+document.body.addEventListener("dragend", handleDragEnd);
+document.body.addEventListener("dragover", (e) => e.preventDefault());
+
+document.body.addEventListener("drop", handleDrop);
